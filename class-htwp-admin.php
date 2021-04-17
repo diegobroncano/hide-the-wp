42e11742f183ff890e15083c1d1c92e49b4d54d8
Diego Hernández <diego@hernandezf.com>
2021-04-17 21:52:32 +0200

    Add support to internationalization

--- class-htwp-admin.php
+++ class-htwp-admin.php
@@ -1,12 +1,28 @@
 <?php
 
-
+/**
+ * Class to handle admin options and menus.
+ *
+ * @package Hide_The_WP
+ */
 class HTWP_admin
 {
 
+	/**
+	 * Initialize support for Hide The WP options in WordPress backend.
+	 */
 	public function load()
 	{
+		// Set up internationalization
+		add_action( 'plugin_loaded', array($this, 'load_textdomain') );
+	}
 
+	/**
+	 * Load the plugin textdomain.
+	 */
+	public function load_textdomain()
+	{
+		load_plugin_textdomain( HTWP_TEXTDOMAIN, false, HTWP_PLUGIN_DIR.'/languages' );
 	}
 
 }
