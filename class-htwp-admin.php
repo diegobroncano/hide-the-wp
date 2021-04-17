6f8c0b4f549ef5a2e461e1c16048c0b5796a1b38
Diego Hernández <diego@hernandezf.com>
2021-04-18 01:46:07 +0200

    FEAT: Add menu entry

--- class-htwp-admin.php
+++ class-htwp-admin.php
@@ -7,14 +7,16 @@
  */
 class HTWP_admin
 {
-
 	/**
 	 * Initialize support for Hide The WP options in WordPress backend.
 	 */
 	public function load()
 	{
-		// Set up internationalization
+		// Set up internationalization.
 		add_action( 'plugin_loaded', array($this, 'load_textdomain') );
+
+		// Add menu entry.
+		add_action( 'admin_menu', array($this, 'add_menu') );
 	}
 
 	/**
@@ -23,6 +25,32 @@ class HTWP_admin
 	public function load_textdomain()
 	{
 		load_plugin_textdomain( HTWP_TEXTDOMAIN, false, HTWP_PLUGIN_DIR.'/languages' );
+	}
+
+	/**
+	 * Add a Settings submenu entry.
+	 */
+	public function add_menu()
+	{
+		add_options_page(
+			__( 'Hide WordPress Settings', HTWP_TEXTDOMAIN ),
+			__( 'Hide WordPress', HTWP_TEXTDOMAIN ),
+			'manage_options',
+			'hide_the_wp',
+			array($this, 'menu_page')
+		);
+	}
+
+	/**
+	 * The menu page HTML.
+	 */
+	public function menu_page() {
+		?>
+		<div class="wrap">
+			<h1><?php esc_html_e('Hide WordPress Settings', HTWP_TEXTDOMAIN); ?></h1>
+			<p><?php esc_html_e('Change what you want to hide of WordPress. We recommend to select as much options as possible, but always check compatibility issues.', HTWP_TEXTDOMAIN); ?></p>
+		</div>
+		<?php
 	}
 
 }
