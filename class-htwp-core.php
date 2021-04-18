9339bb6705d7eebe1e768d962fabbe0abccf9857
Diego Hernández <diego@hernandezf.com>
2021-04-18 02:04:24 +0200

    FEAT: Add hide generator meta tag logic

--- class-htwp-core.php
+++ class-htwp-core.php
@@ -1,10 +1,28 @@
 <?php
 
-
+/**
+ * Class to handle actual WordPress hide options.
+ */
 class HTWP_core
 {
+
+	/**
+	 * Check every option and execute which are selected.
+	 */
 	public function load()
 	{
+		// Remove the generator meta tag.
+		if ( get_option('hide_generator_meta') ) {
+			$this->hide_generator_meta();
+		}
+	}
 
+	/**
+	 * Remove the generator meta tag.
+	 * WordPress by default adds a meta tag in your website identifying itself.
+	 */
+	private function hide_generator_meta()
+	{
+		remove_action( 'wp_head', 'wp_generator' );
 	}
 }
