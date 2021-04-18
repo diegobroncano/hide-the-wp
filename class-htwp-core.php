09419c45df7a1d427dd01205290397bf42f8f21c
Diego Hernández <diego@hernandezf.com>
2021-04-18 02:08:34 +0200

    FEAT: Add hide files versions logic

--- class-htwp-core.php
+++ class-htwp-core.php
@@ -15,6 +15,27 @@ class HTWP_core
 		if ( get_option('hide_generator_meta') ) {
 			$this->hide_generator_meta();
 		}
+
+		// Remove files version.
+		if ( get_option('hide_files_versions') ) {
+			add_filter('style_loader_src', array($this, 'hide_file_version'), 9999, 2);
+			add_filter('script_loader_src', array($this, 'hide_file_version'), 9999, 2);
+		}
+	}
+
+	/**
+	 * Remove for every linked file its version.
+	 * That version is included for cache preventing problems, but you would probably like to hide it.
+	 *
+	 * @param mixed $src File URL
+	 * @return mixed
+	 */
+	public function hide_file_version( $src )
+	{
+		if( strpos( $src, '?ver=' ) ) {
+			$src = remove_query_arg('ver', $src);
+		}
+		return $src;
 	}
 
 	/**
